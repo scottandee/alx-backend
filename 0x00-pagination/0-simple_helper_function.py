a3d56e24dbe0486aa92eaecd7eb26631a4b17e47
Andee WSL <andeeolayinkascott@gmail.com>
2023-12-30 15:26:07 +0100

    Task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This script contains a function that calculates
+the range of indexes to return in a list for the
+given pagination parameters
+"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """This function takes two pagination parameters
+    as argument and returns a tuple containing the
+    start and end index for the pagination of a certain
+    list
+    """
+    end: int = page * page_size
+    start: int = end - page_size
+    return (start, end)
