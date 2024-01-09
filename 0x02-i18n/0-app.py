a0c7faae47ce1fbcb9fd4cd505dd9364538dc7af
Andee WSL <andeeolayinkascott@gmail.com>
2024-01-09 17:11:27 +0100

    Task 1

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    """This route returns the
+    index.html file
+    """
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
