1ffea527b399fa2e3cf1d124910da0630a688956
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-03-06 03:46:09 +0100

    Task 1-4

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,21 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+
+client.on('error', err => console.log('Redis client not connected to the server:', err));
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const result = await getAsync(schoolName);
+  console.log(result);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
